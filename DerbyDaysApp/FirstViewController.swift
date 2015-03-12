a19eda734ac212b91c47c95fe102683797e5823a
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-03-11 22:06:29 -0400

    Initial Commit

---
+++ DerbyDaysApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DerbyDaysApp
+//
+//  Created by Ryan Sickles on 3/11/15.
+//  Copyright (c) 2015 sickles.ryan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
