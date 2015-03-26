3cdc969ca7341a94bce3b4684c5dcdc51f976adf
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-03-26 16:39:30 -0400

    Auto layout for scoreboard done v1

--- DerbyDaysApp/FirstViewController.swift
+++ DerbyDaysApp/FirstViewController.swift
@@ -15,12 +15,16 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBOutlet var team_1: UILabel!
     
+    @IBOutlet var team_2: UILabel!
     @IBOutlet var amount_raised: UILabel!
+    @IBOutlet var team_3: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     @IBAction func showDonatePage(sender: UIButton) {
         let webViewController = SVModalWebViewController(address: "https://www.gofundme.com/cmuderbydays/donate")
@@ -37,6 +41,9 @@ class FirstViewController: UIViewController {
             // The find succeeded.
                 println(object.objectForKey("amount_raised"))
             self.amount_raised.text = "$ " + (object.objectForKey("amount_raised") as? String)!
+            self.team_1.text = "1. " + (object.objectForKey("team_1") as? String)! + "   " + (object.objectForKey("points_1") as? String)!
+                self.team_2.text = "2. " + (object.objectForKey("team_2") as? String)! + "   " + (object.objectForKey("points_2") as? String)!
+                self.team_3.text = "3. " + (object.objectForKey("team_3") as? String)! + "   " + (object.objectForKey("points_3") as? String)!
             println("Successfully retrieved the object.")
         }
     }
